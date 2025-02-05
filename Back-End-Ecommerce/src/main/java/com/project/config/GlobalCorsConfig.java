f3528a26baae1bc95707004286adfd0c1b0e0cb9
Pramod858 <pramodbadiger45@gmail.com>
2025-02-05 12:26:23 +0530

    Code Updated

--- Back-End-Ecommerce/src/main/java/com/project/config/GlobalCorsConfig.java
+++ Back-End-Ecommerce/src/main/java/com/project/config/GlobalCorsConfig.java
@@ -1,16 +1,20 @@
 package com.project.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class GlobalCorsConfig implements WebMvcConfigurer {
+	
+	@Value("${angular.url}")
+    private String angularUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(angularUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
