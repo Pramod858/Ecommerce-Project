c7b0fedaee03b7b567fc31e09921c986acd44dd3
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2025-02-09 16:43:25 +0530

    code updated

--- Back-End-Ecommerce/src/main/java/com/project/config/GlobalCorsConfig.java
+++ Back-End-Ecommerce/src/main/java/com/project/config/GlobalCorsConfig.java
@@ -14,7 +14,7 @@ public class GlobalCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(angularUrl)
+                .allowedOrigins(*)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
