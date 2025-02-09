4643b593f071392de098c2b1cd9ed12b20cf7df7
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2025-02-09 16:36:58 +0530

    code updated

--- Front-End-Ecommerce/src/environments/environment.development.ts
+++ Front-End-Ecommerce/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://host.docker.internal:8080'
+    apiUrl: 'http://springboot-app:8080'
 };
--- Front-End-Ecommerce/src/environments/environment.ts
+++ Front-End-Ecommerce/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://host.docker.internal:8080'
+    apiUrl: 'http://springboot-app:8080'
 };
