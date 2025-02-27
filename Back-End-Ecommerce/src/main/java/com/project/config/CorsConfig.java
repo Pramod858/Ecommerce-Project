2f27ce5fc7e2b5d6267e9c22e8417035e659c9a5
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2025-02-27 09:40:51 +0530

    Update CorsConfig.java

--- Back-End-Ecommerce/src/main/java/com/project/config/CorsConfig.java
+++ Back-End-Ecommerce/src/main/java/com/project/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList(angularUrl));
+        config.setAllowedOrigins(Arrays.asList("*"));
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
